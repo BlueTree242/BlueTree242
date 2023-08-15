f7f5de9b3ac021c51ba6cd261e5e3012ec176068
BlueTree242 <yahiamsm2@gmail.com>
2023-08-15 10:10:46 +0300

    make this look better on devices in small width range

--- src/components/TopContainer.tsx
+++ src/components/TopContainer.tsx
@@ -2,14 +2,14 @@ import tw from "twin.macro";
 
 export default function TopContainer() {
     return <>
-        <div css={tw`md:h-screen flex max-md:mb-[5rem]`}>
-            <div css={tw`md:m-auto md:flex block`}>
-                <div css={tw`max-md:w-full max-md:flex`}>
-                    <img src="/profile_picture.png" css={tw`md:w-[50vh] md:h-[50vh] md:my-auto w-1/3 mx-auto mb-5 select-none pointer-events-none`} />
+        <div css={tw`lg:h-screen flex max-lg:mb-[5rem]`}>
+            <div css={tw`lg:m-auto lg:flex block`}>
+                <div css={tw`max-lg:w-full max-lg:flex`}>
+                    <img src="/profile_picture.png" css={tw`lg:w-[50vh] lg:h-[50vh] lg:my-auto w-1/3 mx-auto mb-5 select-none pointer-events-none`} />
                 </div>
-                <div css={tw`md:w-[60vh] w-full md:ml-3`}>
+                <div css={tw`lg:w-[60vh] w-full lg:ml-3`}>
                     <h1 css={tw`text-center text-blue-700 text-5xl`}>BlueTree242</h1>
-                    <p css={tw`mt-5 text-lg text-center md:text-left`}>
+                    <p css={tw`mt-5 text-lg text-center lg:text-left`}>
                         Hello there! I am BlueTree242, I code programs as a hobby.
                         I mostly make Minecraft/Discord related stuff.<br/><br/>
                         Some of my projects are open source and available on github! I know Java, JS, TS, React.JS, Node.js.
